048f1d53519888055da57d32e65f0b6c99d11f93
Sarah Gilmore <sgilmore@mathworks.com>
2025-06-01 21:04:48 -0400

    Add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// TODO: Add Cmake support... 
+// Just manually use clang++ for now.
+
+int main() {
+    std::cout << "Let's write a ray tracer" << std::endl;
+    return 0;
+}
